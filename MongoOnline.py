d256d4872d2cbdf8f0d5cbf1ee6d1675e0c0f9ee
WisdomDaPhoenix <hotwizzytron@gmail.com>
2025-01-21 12:01:26 +0100

    updated MongoOnline.py

--- MongoOnline.py
+++ MongoOnline.py
@@ -1,17 +1,12 @@
-def addToMongoDB(student_document):
-    from pymongo.mongo_client import MongoClient
-    from pymongo.server_api import ServerApi
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
+def addToMongoDB(student_document):
     uri = "mongodb+srv://iconsoftwareguru:%40Datron24@dxyserverlessdb.ngiqf.mongodb.net/?retryWrites=true&w=majority&appName=DXYServerlessDB"
     client = MongoClient(uri, server_api=ServerApi('1'))
-
     db = client['dxy']
     students = db['students']
     result = students.insert_one(student_document)
     if result:
         return f"Your assigned Client ID: {result.inserted_id}"
     return f"You have no assigned Client ID. Contact Admin"
-
-#
-# if __name__=="__main__":
-#     print(addToMongoDB())
